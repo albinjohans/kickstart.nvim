9919ff545dc98461e326513b3f14031a5faaada7
Albin Johansson <albin@albinjohansson.com>
2025-01-26 15:14:50 +0100

    Change keymap for telescope help command to work on ISO-keyboard.

--- init.lua
+++ init.lua
@@ -297,11 +297,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-h>'] = 'which_key' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
